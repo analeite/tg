bf22758e127242eec107b3a5e88b513c1008030f
Alex Carreira <alex@develop.season.local>
2017-04-06 21:07:53 -0300

    -- First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cgtec_appman
+============
+
+A Symfony project created on March 31, 2017, 10:48 am.
---
+++ src/SysadminBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SysadminBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sys_user")
+ */
+class User extends BaseUser {
+    
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="SysadminBundle\Entity\Group")
+     * @ORM\JoinTable(name="sys_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+    
+    public function __construct() {
+        parent::__construct();
+    }
+}
